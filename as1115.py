b91b00c04b9cebfc828e07a7e1400f2034cabae7
leehands <leehands83@icloud.com>
2021-08-09 19:39:18 +0900

    Correct FND was installed

--- as1115.py
+++ as1115.py
@@ -1,10 +1,17 @@
 #################################
 ## Python Module from Leehands ##
 #################################
-
+from gpiozero import LED
 from time import sleep
 import smbus
+import time
 
+
+ld_AM_CA = LED(7)
+ld_PM_CA = LED(8)
+ld_ALARM_CA = LED(21)
+ld_LCUC_AN = LED(16)
+ld_LCUC_CA = LED(12)
 
 CTR_SHUTDOWN =	0x0C
 CTR_DECODEEN =	0x09
@@ -12,16 +19,21 @@ CTR_INTENSITY =	0x0A
 CTR_SCANLIMIT =	0x0B
 CTR_FEATURE =	0x0E
 
-FND_4 = 0x01
-FND_3 = 0x06
-FND_2 = 0x05
-FND_1 = 0x07
+
+FND_HR_10 = 0x01
+FND_HR_01 = 0x02
+FND_MN_10 = 0x03
+FND_MN_01 = 0x04
+
 
 class AS1115:
     def __init__(self,addr):
         self.addr = addr
         self.bus = smbus.SMBus(1) # 0 = /dev/i2c-0 , 1 = /dev/i2c-1
         self.cnt = 0
+        self.now = time.localtime()
+        ld_LCUC_AN.off()     # always GND
+
     
         self.bus.write_byte_data(self.addr,CTR_FEATURE, 0x02)
         sleep(0.01)
@@ -45,10 +57,45 @@ class AS1115:
         else :
             self.cnt = self.cnt + 1
 
-        self.bus.write_byte_data(self.addr,FND_1, self.cnt)
-        self.bus.write_byte_data(self.addr,FND_2, self.cnt%10)
-        self.bus.write_byte_data(self.addr,FND_3, self.cnt%100)
-        self.bus.write_byte_data(self.addr,FND_4, self.cnt%1000)
+        self.bus.write_byte_data(self.addr,FND_HR_10, 1)
+        self.bus.write_byte_data(self.addr,FND_HR_01, 2)
+        self.bus.write_byte_data(self.addr,FND_MN_10, 3)
+        self.bus.write_byte_data(self.addr,FND_MN_01, 4)
+
+    def DisplayLocalTime(self):
+        self.now = time.localtime()
+               
+        if self.now.tm_hour > 12 :
+            ld_PM_CA.on()
+            ld_AM_CA.off()
+            dis_hour = self.now.tm_hour - 12
+            
+            self.bus.write_byte_data(self.addr,FND_HR_01, dis_hour%10)
+            if dis_hour < 10:   
+                dis_hour = 0x0F
+            else :
+                dis_hour = dis_hour/10
+            self.bus.write_byte_data(self.addr,FND_HR_10, dis_hour)
+        else :
+            ld_PM_CA.off()
+            ld_AM_CA.on()
+            dis_hour = self.now.tm_hour
+            self.bus.write_byte_data(self.addr,FND_HR_10, dis_hour/10)
+            self.bus.write_byte_data(self.addr,FND_HR_01, dis_hour%10)
+
+        self.bus.write_byte_data(self.addr,FND_MN_10, self.now.tm_min/10)
+        self.bus.write_byte_data(self.addr,FND_MN_01, self.now.tm_min%10)
+
+        if(self.now.tm_sec % 2):
+            ld_ALARM_CA.on()
+            ld_LCUC_CA.on() 
+        else:
+            ld_ALARM_CA.off()
+            ld_LCUC_CA.off() 
+    def Brighless(self,adc_input)
+        
+
+
 
 
 
--- i2ctest.py
+++ i2ctest.py
@@ -3,6 +3,7 @@ from time import sleep
 import smbus
 import adc081
 import as1115
+import time
 
 bus = smbus.SMBus(1) # 0 = /dev/i2c-0 , 1 = /dev/i2c-1
 
@@ -10,21 +11,18 @@ buff = 0x03
 ld_PWR = LED(17)
 ld_STAT = LED(27)
 ld_SYNC = LED(22)
+ld_DP_CA = LED(20)
 
 adc = adc081.ADC081(0x52)
 asfnd = as1115.AS1115(0x00)
 
+
+ld_DP_CA.off()
+
+
 while True:
     adc.readVoltage()
-    sleep(0.25)
-    asfnd.AutoCount()
-    ld_PWR.on()
-    ld_STAT.off()
-    ld_SYNC.on()
-    sleep(0.25)
-    ld_PWR.off()
-    ld_STAT.on()
-    ld_SYNC.off()
+    asfnd.DisplayLocalTime()
     sleep(0.25)
 
 
